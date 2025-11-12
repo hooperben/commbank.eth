8f359f0837eda1036a96efb1aa915213eec696c2
hooperben <s4394947@gmail.com>
2025-11-12 10:13:57 +1000

    fix: decreased number of optimisation runs

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 100,
           },
         },
       },
@@ -24,7 +24,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 100,
           },
         },
       },
@@ -33,7 +33,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 100,
           },
         },
       },
