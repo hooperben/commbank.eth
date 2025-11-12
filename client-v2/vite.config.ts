995539606abf355ac69e4164b60acf8bf01d9352
hooperben <s4394947@gmail.com>
2025-11-12 12:36:46 +1000

    feat: updated client-v2 to have correct settings to import noir

--- client-v2/vite.config.ts
+++ client-v2/vite.config.ts
@@ -3,13 +3,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), nodePolyfills()],
+  optimizeDeps: {
+    exclude: ["@aztec/bb.js", "@noir-lang/noir_js"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      pino: "pino/browser.js",
     },
+    dedupe: ["react", "react-dom"],
   },
 });
