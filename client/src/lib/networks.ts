883b1133693c08aeb449d6bb2b1d756c92d6c8d9
hooperben <s4394947@gmail.com>
2025-12-02 16:00:43 +1000

    feat: moved nullifier to bytes32 type too

--- client/src/lib/networks.ts
+++ client/src/lib/networks.ts
@@ -2,11 +2,11 @@
 export const SUPPORTED_NETWORKS: Record<number, any> = {
   11155111: {
     rpc: "https://eth-sepolia.g.alchemy.com/v2/Xq2kgRj8MHmXSE3sbThrHfr9R4-5HPWg",
-    ZKTranscriptLib: "0x751b0fd43e81c7eA4B0Edc808c7A8AeB9AC2dAC8",
-    DepositVerifier: "0xc56287e24913BC667fBCC0a38e5c8F4c93357632",
-    TransferVerifier: "0x2Adbe9B0110704ce793d6Eb1997e57D1b59E1EEa",
-    WithdrawVerifier: "0x2F2Dbe3059C99DAF2318D188161A4cf12ff49EFc",
-    CommBankDotEth: "0x6813974e8A965fE09efEEc1cD4DC6D8FBa0831dC",
+    ZKTranscriptLib: "0xD5cFAE8401c24911eEf160170Aa0611F882e213c",
+    DepositVerifier: "0x5Cb312deE3177BA513a9C9894fDeD948b350701C",
+    TransferVerifier: "0xc17D9AD9bb33ECAe3F308978dc4dA192933a0480",
+    WithdrawVerifier: "0xa29FfB2093D5C05066AF4d77c703eaCee0BE8e18",
+    CommBankDotEth: "0x8385753132c072d15c9F861B93D4827aaB549EFc",
     FourDEC: "0x840feC4Efbd4764edAA237E78757cE59CeA3Db26",
     USDC: "0x237eEeE66266c72DBb7Ee2Aa84811666cE4EB815",
   },
