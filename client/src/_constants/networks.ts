e01e041120d5f7e3cdfcecc963c6fb76ecb342b3
hooperben <s4394947@gmail.com>
2026-01-13 08:08:44 +1000

    chore: updated sepolia to new deployment address

--- client/src/_constants/networks.ts
+++ client/src/_constants/networks.ts
@@ -2,7 +2,7 @@
 export const SUPPORTED_NETWORKS: Record<number, any> = {
   11155111: {
     rpc: "https://eth-sepolia.g.alchemy.com/v2/Xq2kgRj8MHmXSE3sbThrHfr9R4-5HPWg",
-    CommBankDotEth: "0x582E8C09955727112014B50A4eB3978697CDCB77",
+    CommBankDotEth: "0x5C0EE420423cB0d7d39d413c313D8B0fe3bD0789",
     name: "Ethereum",
   },
   1: {
